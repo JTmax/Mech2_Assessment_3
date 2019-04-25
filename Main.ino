d21d81c2bec87810f757012e8f170ccae8819c86
JTmax <jason.travis2@gmail.com>
2019-04-25 17:35:59 +1000

    Added core functions for all sub moules

--- Main.ino
+++ Main.ino
@@ -1,3 +1,67 @@
+#include "Main.h"
+
+void IR() //For Hope
+{
+
+
+}
+
+void Motor(int Speed, int Direction)
+{
+
+
+}
+
+void Coms() //For Yashwin
+{
+
+}
+
+void Modes()
+{
+
+}
+
+void ConstantSpeed()
+{
+    Motor(150, CC);
+}
+
+void FusionMode()
+{
+
+}
+
+
+void GloveData()
+{
+
+}
+
+void Menu(int mode)
+{
+    switch (mode)
+    {
+        //Constant Speed Mode
+        case 1:
+            
+
+            ConstantSpeed();
+            break;
+        //Fusion mode
+        case 2:
+
+            FusionMode();
+            break;
+        default:
+            break;
+    }
+
+}
+
+
+
+
 void setup()
 {
 
@@ -6,6 +70,10 @@ void setup()
 
 void loop()
 {
+    Coms();
+    IR();
 
+    Menu();
 
 }
+
